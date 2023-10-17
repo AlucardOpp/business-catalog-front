4e5aac33fbc0a9217957c814fa4cc269f5fd79ed
Vladislav Knyazev <knyazev.activ@yandex.ru>
2023-10-17 18:24:25 +0300

    Чекбоксы на карте

--- source/js/modules/map/map-init.js
+++ source/js/modules/map/map-init.js
@@ -4,59 +4,37 @@ const mapInit = () => {
     zoom: 9,
   });
 
-  const blueCoords = [
-    [54.42810563616309, 43.75812457039163],
-    [54.0333466411536, 44.407135788664185]
-  ];
-
   const redCoords = [
+    [54.422251814108, 45.322027847298294],
+    [54.43186044174936, 45.332327529915496],
+    [54.415844804920326, 45.346060440071746],
     [54.32604456497944, 43.32578759179032],
     [54.09106653463098, 43.22279076561844]
   ];
 
-  const closeRedCoords = [
-    [54.422251814108, 45.322027847298294],
-    [54.43186044174936, 45.332327529915496],
-    [54.415844804920326, 45.346060440071746]
-  ];
-
-  const closeBlueCoords = [
+  const blueCoords = [
     [54.318013214108994, 44.4383150787436],
-    [54.3035592216153, 44.46440760804048]
+    [54.3035592216153, 44.46440760804048],
+    [54.42810563616309, 43.75812457039163],
+    [54.0333466411536, 44.407135788664185]
   ];
 
-  const blueCollection = new ymaps.GeoObjectCollection({}, {
-    iconLayout: 'default#image',
-    iconImageHref: '../../../img/svg/blue-marker.svg',
-    iconImageSize: [29, 39],
-  });
-
-  for (let i = 0; i < blueCoords.length; i++) {
-    blueCollection.add(new ymaps.Placemark(blueCoords[i]));
-  }
-
-  const redCollection = new ymaps.GeoObjectCollection({}, {
-    iconLayout: 'default#image',
-    iconImageHref: '../../../img/svg/red-marker.svg',
-    iconImageSize: [29, 39],
-  });
-
-  for (let i = 0; i < redCoords.length; i++) {
-    redCollection.add(new ymaps.Placemark(redCoords[i]));
-  }
+  const getRedOptions = () => {
+    return {
+      iconLayout: 'default#image',
+      iconImageHref: '../../../img/svg/red-marker.svg',
+      iconImageSize: [29, 39],
+    };
+  };
 
   const redObjects = [];
-  for (let i = 0; i < closeRedCoords.length; i++) {
-    redObjects[i] = new ymaps.GeoObject({
-      geometry: {
-        type: 'Point',
-        coordinates: closeRedCoords[i],
-      },
-    });
+  for (let i = 0; i < blueCoords.length; i++) {
+    redObjects[i] = new ymaps.Placemark(redCoords[i], {}, getRedOptions());
   }
 
+
   const iconNumberLayout = ymaps.templateLayoutFactory.createClass(
-    '<div style="color: #000000; font-weight: 700; font-family: Montserrat; font-size: 24px; line-height: 110%; margin-top: 11px;">{{ properties.geoObjects.length }}</div>'
+      '<div style="color: #000000; font-weight: 700; font-family: Montserrat; font-size: 24px; line-height: 110%; margin-top: 11px;">{{ properties.geoObjects.length }}</div>'
   );
 
   const redClusterer = new ymaps.Clusterer({
@@ -77,14 +55,17 @@ const mapInit = () => {
   });
   redClusterer.add(redObjects);
 
+  const getBlueOptions = () => {
+    return {
+      iconLayout: 'default#image',
+      iconImageHref: '../../../img/svg/blue-marker.svg',
+      iconImageSize: [29, 39],
+    };
+  };
+
   const blueObjects = [];
-  for (let i = 0; i < closeBlueCoords.length; i++) {
-    blueObjects[i] = new ymaps.GeoObject({
-      geometry: {
-        type: 'Point',
-        coordinates: closeBlueCoords[i],
-      },
-    });
+  for (let i = 0; i < blueCoords.length; i++) {
+    blueObjects[i] = new ymaps.Placemark(blueCoords[i], {}, getBlueOptions());
   }
 
   const blueClusterer = new ymaps.Clusterer({
@@ -105,10 +86,28 @@ const mapInit = () => {
   });
   blueClusterer.add(blueObjects);
 
-  myMap.geoObjects.add(blueCollection);
-  myMap.geoObjects.add(redCollection);
   myMap.geoObjects.add(redClusterer);
   myMap.geoObjects.add(blueClusterer);
+
+  const industrialCategory = document.querySelector('#industrial');
+  const agroCategory = document.querySelector('#agro');
+
+  industrialCategory.addEventListener('change', () => {
+    if (industrialCategory.checked === true) {
+      myMap.geoObjects.add(redClusterer);
+    } else {
+      myMap.geoObjects.remove(redClusterer);
+    }
+  });
+
+  agroCategory.addEventListener('change', () => {
+    if (agroCategory.checked === true) {
+      myMap.geoObjects.add(blueClusterer);
+    } else {
+      myMap.geoObjects.remove(blueClusterer);
+    }
+  });
+
 
   myMap.controls.remove('searchControl');
   myMap.controls.remove('trafficControl');
