0550fdb265f87794615819ca9c0ebebcde858239
Vladislav Knyazev <knyazev.activ@yandex.ru>
2023-10-18 12:25:39 +0300

    Фикс багов 4-6

--- source/js/modules/parallax/init-parallax.js
+++ source/js/modules/parallax/init-parallax.js
@@ -9,6 +9,7 @@ const initParallax = () => {
   const rellax = new Rellax('.rellax');
   const breakpointTb = window.matchMedia('(max-width: 1563px)');
   const headerStaticRight = document.querySelector('.header__static-right');
+  const headerStaticRightImg = document.querySelector('.header__static-right img');
 
   const resizeObserver = new ResizeObserver((entries, observer) => {
     for (let entry of entries) {
@@ -20,11 +21,15 @@ const initParallax = () => {
       } else {
         rellax.refresh();
         document.onscroll = () => {
-          let percent = (screen.availHeight - window.scrollY / 2) / screen.availHeight;
+          let percent = (screen.availHeight - window.scrollY / 0.8) / screen.availHeight;
+          let scale = 1;
+          scale += window.scrollY * 0.5 / screen.availHeight;
           if (percent <= 0) {
             headerStaticRight.style.opacity = 0;
+            headerStaticRightImg.style.transform = 'scale(1)';
           } else {
             headerStaticRight.style.opacity = percent;
+            headerStaticRightImg.style.transform = 'scale(' + scale + ')';
           }
         };
       }
