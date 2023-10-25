5093603589c92d7e69009866514a3b626d8c59b8
Vladislav Knyazev <knyazev.activ@yandex.ru>
2023-10-25 10:52:43 +0300

    Фикс меток на карте

--- source/js/modules/map/map-init.js
+++ source/js/modules/map/map-init.js
@@ -33,30 +33,22 @@ const mapInit = () => {
   });
 
 
-  let redCoords = [
-    [54.422251814108, 45.322027847298294],
-    [54.43186044174936, 45.332327529915496],
-    [54.415844804920326, 45.346060440071746],
-    [54.32604456497944, 43.32578759179032]
-  ];
+  let redCoords = [];
 
 
   if (map.getAttribute('data-red-coords').length > 0) {
     redCoords = JSON.parse(map.getAttribute('data-red-coords'));
   }
 
-  let blueCoords = [
-    [54.318013214108994, 44.4383150787436],
-    [54.3035592216153, 44.46440760804048],
-    [54.42810563616309, 43.75812457039163],
-    [54.0333466411536, 44.407135788664185]
-  ];
+  let blueCoords = [];
 
   if (map.getAttribute('data-blue-coords').length > 0) {
     blueCoords = JSON.parse(map.getAttribute('data-blue-coords'));
   }
 
-  blueCoords = JSON.parse(map.getAttribute('data-blue-coords'));
+  const iconNumberLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #000000; font-weight: 700; font-family: Montserrat; font-size: 24px; line-height: 110%; margin-top: 11px;">{{ properties.geoObjects.length }}</div>'
+  );
 
   const getRedOptions = () => {
     return {
@@ -66,89 +58,91 @@ const mapInit = () => {
     };
   };
 
-  const redObjects = [];
-  for (let i = 0; i < redCoords.length; i++) {
-    redObjects[i] = new ymaps.Placemark(redCoords[i], {}, getRedOptions());
+  if (redCoords.length > 0) {
+    const redObjects = [];
+
+    for (let i = 0; i < redCoords.length; i++) {
+      redObjects[i] = new ymaps.Placemark(redCoords[i], {}, getRedOptions());
+    }
+
+    const redClusterer = new ymaps.Clusterer({
+      clusterIcons: [
+        {
+          href: '/local/templates/main/img/svg/red-clusterer-less-10.svg',
+          size: [48, 48],
+          offset: [0, 0],
+        },
+        {
+          href: '/local/templates/main/img/svg/red-clusterer-more-10.svg',
+          size: [56, 56],
+          offset: [0, 0],
+        }
+      ],
+      clusterNuymbers: [10],
+      clusterIconContentLayout: iconNumberLayout,
+    });
+    redClusterer.add(redObjects);
+
+    myMap.geoObjects.add(redClusterer);
+
+    const industrialCategory = document.querySelector('#industrial');
+
+    if (industrialCategory) {
+      industrialCategory.addEventListener('change', () => {
+        if (industrialCategory.checked === true) {
+          myMap.geoObjects.add(redClusterer);
+        } else {
+          myMap.geoObjects.remove(redClusterer);
+        }
+      });
+    }
   }
 
-
-  const iconNumberLayout = ymaps.templateLayoutFactory.createClass(
-      '<div style="color: #000000; font-weight: 700; font-family: Montserrat; font-size: 24px; line-height: 110%; margin-top: 11px;">{{ properties.geoObjects.length }}</div>'
-  );
-
-  const redClusterer = new ymaps.Clusterer({
-    clusterIcons: [
-      {
-        href: '/local/templates/main/img/svg/red-clusterer-less-10.svg',
-        size: [48, 48],
-        offset: [0, 0],
-      },
-      {
-        href: '/local/templates/main/img/svg/red-clusterer-more-10.svg',
-        size: [56, 56],
-        offset: [0, 0],
-      }
-    ],
-    clusterNuymbers: [10],
-    clusterIconContentLayout: iconNumberLayout,
-  });
-  redClusterer.add(redObjects);
-
-  const getBlueOptions = () => {
-    return {
-      iconLayout: 'default#image',
-      iconImageHref: '/local/templates/main/img/svg/blue-marker.svg',
-      iconImageSize: [29, 39],
+  if (blueCoords.length > 0) {
+    const getBlueOptions = () => {
+      return {
+        iconLayout: 'default#image',
+        iconImageHref: '/local/templates/main/img/svg/blue-marker.svg',
+        iconImageSize: [29, 39],
+      };
     };
-  };
 
-  const blueObjects = [];
-  for (let i = 0; i < blueCoords.length; i++) {
-    blueObjects[i] = new ymaps.Placemark(blueCoords[i], {}, getBlueOptions());
-  }
+    const blueObjects = [];
+    for (let i = 0; i < blueCoords.length; i++) {
+      blueObjects[i] = new ymaps.Placemark(blueCoords[i], {}, getBlueOptions());
+    }
 
-  const blueClusterer = new ymaps.Clusterer({
-    clusterIcons: [
-      {
-        href: '/local/templates/main/img/svg/blue-clusterer-less-10.svg',
-        size: [48, 48],
-        offset: [0, 0],
-      },
-      {
-        href: '/local/templates/main/img/svg/blue-clusterer-more-10.svg',
-        size: [56, 56],
-        offset: [0, 0],
-      }
-    ],
-    clusterNuymbers: [10],
-    clusterIconContentLayout: iconNumberLayout,
-  });
-  blueClusterer.add(blueObjects);
-
-  myMap.geoObjects.add(redClusterer);
-  myMap.geoObjects.add(blueClusterer);
-
-  const industrialCategory = document.querySelector('#industrial');
-  const agroCategory = document.querySelector('#agro');
-
-  if (industrialCategory) {
-    industrialCategory.addEventListener('change', () => {
-      if (industrialCategory.checked === true) {
-        myMap.geoObjects.add(redClusterer);
-      } else {
-        myMap.geoObjects.remove(redClusterer);
-      }
+    const blueClusterer = new ymaps.Clusterer({
+      clusterIcons: [
+        {
+          href: '/local/templates/main/img/svg/blue-clusterer-less-10.svg',
+          size: [48, 48],
+          offset: [0, 0],
+        },
+        {
+          href: '/local/templates/main/img/svg/blue-clusterer-more-10.svg',
+          size: [56, 56],
+          offset: [0, 0],
+        }
+      ],
+      clusterNuymbers: [10],
+      clusterIconContentLayout: iconNumberLayout,
     });
-  }
+    blueClusterer.add(blueObjects);
 
-  if (agroCategory) {
-    agroCategory.addEventListener('change', () => {
-      if (agroCategory.checked === true) {
-        myMap.geoObjects.add(blueClusterer);
-      } else {
-        myMap.geoObjects.remove(blueClusterer);
-      }
-    });
+    myMap.geoObjects.add(blueClusterer);
+
+    const agroCategory = document.querySelector('#agro');
+
+    if (agroCategory) {
+      agroCategory.addEventListener('change', () => {
+        if (agroCategory.checked === true) {
+          myMap.geoObjects.add(blueClusterer);
+        } else {
+          myMap.geoObjects.remove(blueClusterer);
+        }
+      });
+    }
   }
 };
 
